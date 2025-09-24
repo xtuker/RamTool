46c2de3677bbac78f34f12663501e33a25ea7dca
Alexander Falkovsky <alr.fkv@gmail.com>
2025-09-24 19:34:08 +0300

    strong names

--- RamTool.Manager/Imports.cs
+++ RamTool.Manager/Imports.cs
@@ -12,25 +12,25 @@ internal partial class Imports
     [return: MarshalAs(UnmanagedType.Bool)]
     internal static partial bool LookupPrivilegeValue(string? host, string name, ref long pluid);
 
-    [LibraryImport("advapi32.dll", SetLastError = true)]
+    [LibraryImport("advapi32.dll", EntryPoint = "AdjustTokenPrivileges", SetLastError = true)]
     [return: MarshalAs(UnmanagedType.Bool)]
     internal static partial bool AdjustTokenPrivileges(IntPtr htok, [MarshalAs(UnmanagedType.Bool)] bool disall, ref TokPriv1Luid newst, int len, IntPtr prev, IntPtr relen);
 
-    [LibraryImport("ntdll.dll")]
+    [LibraryImport("ntdll.dll", EntryPoint = "NtSetSystemInformation")]
     internal static partial uint NtSetSystemInformation(int InfoClass, IntPtr Info, int Length);
 
-    [LibraryImport("psapi.dll")]
+    [LibraryImport("psapi.dll", EntryPoint = "EmptyWorkingSet")]
     internal static partial int EmptyWorkingSet(IntPtr hwProc);
 
-    [LibraryImport("advapi32.dll", SetLastError = true)]
+    [LibraryImport("advapi32.dll", EntryPoint = "OpenProcessToken", SetLastError = true)]
     [return: MarshalAs(UnmanagedType.Bool)]
     internal static partial bool OpenProcessToken(IntPtr ProcessHandle, uint DesiredAccess, out IntPtr TokenHandle);
 
-    [LibraryImport("advapi32.dll", SetLastError = true)]
+    [LibraryImport("advapi32.dll", EntryPoint = "GetTokenInformation", SetLastError = true)]
     [return: MarshalAs(UnmanagedType.Bool)]
     internal static partial bool GetTokenInformation(IntPtr TokenHandle, TokenInformationClass TokenInformationClass, IntPtr TokenInformation, int TokenInformationLength, out int ReturnLength);
 
-    [LibraryImport("kernel32.dll", SetLastError = true)]
+    [LibraryImport("kernel32.dll", EntryPoint = "CloseHandle", SetLastError = true)]
     [return: MarshalAs(UnmanagedType.Bool)]
     internal static partial bool CloseHandle(IntPtr hObject);
 }
